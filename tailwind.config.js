1d0fb8025d78acc7ed88c0eb3f06c54e48877964
zaid <amonsefra13@gmail.com>
2026-01-23 00:59:58 +0700

    update tailwind setTheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode : "class",
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}", // Tambahkan ts dan tsx di sini
