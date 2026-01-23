f493bc69a86b5b54420e7c1695f4d5a93f3dc0c9
zem-art <amonsefra13@gmail.com>
2026-01-23 14:52:06 +0700

    update icon render types

--- src/common/components/icons/IconRenderer.tsx
+++ src/common/components/icons/IconRenderer.tsx
@@ -1,7 +1,7 @@
-import { getIcon } from "./icon";
+import { getIcon, IconLibrary } from "./icon";
 
 type IconRendererProps = {
-  lib: "fa" | "md" | "ai" | "bs" | "ri" | "fi" | "hi" | "bi" | "ci" | "cg" | "di" | "fc" | "fi" | "gi" | "go" | "gr" | "im" | "io" | "io5" | "lu" | "si" | "sl" | "tb" | "ti" | "vsc";
+  lib: IconLibrary,
   name: string;
   size?: number;
   className?: string;
@@ -10,7 +10,7 @@ type IconRendererProps = {
 export const IconRenderer = ({ lib, name, size = 24, className = "" }: IconRendererProps) => {
   const Icon = getIcon({ lib, name });
 
-  if (!Icon) return <span className="text-main">Icon not found</span>;
+  if (!Icon) return <span className="text-red-600 font-bold uppercase">Icon not found</span>;
 
   return <Icon size={size} className={className} />;
 };
