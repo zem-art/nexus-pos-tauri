935d7a48a1104f2ba1df395eb450ae9df4dd0c07
zem-art <amonsefra13@gmail.com>
2026-01-23 14:52:18 +0700

    update const index.ts

--- src/common/const/index.ts
+++ src/common/const/index.ts
@@ -1,1 +1,18 @@
+// /src/common/const/index.ts
+
 export const name_apps = "nexus"
+export const version_apps = 'V1.0.2'
+export const module_not_release = 'This module is being prepared for the next release. Please wait for updates from the engineering team.'
+export const menus_cashier = [
+    { id: 'pos', title: 'PENJUALAN', desc: 'Transaksi Baru', icon: "HiOutlineShoppingCart" , color: 'bg-blue-600' },
+    { id: 'pembelian', title: 'PEMBELIAN', desc: 'Barang Masuk', icon: 'HiOutlineDownload', color: 'bg-emerald-600' },
+    { id: 'stok', title: 'CEK STOK', desc: 'Sisa Inventaris', icon: 'HiOutlineArchive', color: 'bg-purple-600' },
+    { id: 'member', title: 'MEMBER', desc: 'Data Pelanggan', icon: 'HiOutlineUserGroup', color: 'bg-pink-600' },
+    { id: 'produk', title: 'KATALOG', desc: 'Daftar Harga', icon: 'HiOutlineCollection', color: 'bg-orange-500' },
+    { id: 'topup', title: 'TOP UP', desc: 'Saldo & E-Wallet', icon: 'HiOutlineLightningBolt', color: 'bg-amber-500' },
+    { id: 'promo', title: 'PROMO', desc: 'Voucher & Diskon', icon: 'HiOutlineTicket', color: 'bg-rose-500' },
+    { id: 'riwayat', title: 'RIWAYAT', desc: 'Cetak Struk', icon: 'HiOutlineClipboardList', color: 'bg-slate-600' },
+    { id: 'shift', title: 'SHIFT', desc: 'Buka / Tutup', icon: 'HiOutlineKey', color: 'bg-indigo-600' },
+    { id: 'setting', title: 'SETTING', desc: 'Printer Perangkat & Bantuan Sistem', icon: 'HiOutlineCog', color: 'bg-slate-800' },
+    // { id: 'help', title: 'BANTUAN', desc: 'Panduan Sistem', icon: 'HiOutlineChatAlt2', color: 'bg-cyan-700' },
+];
