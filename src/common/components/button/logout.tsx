71fdeac1a23ef79c2d8708ae7023b8cba14b5cb0
zem-art <amonsefra13@gmail.com>
2026-01-23 14:51:33 +0700

    update logut theme dark

--- src/common/components/button/logout.tsx
+++ src/common/components/button/logout.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { HiOutlineLogout } from 'react-icons/hi';
-import { name_apps } from '../../const';
+import { name_apps, version_apps } from '../../const';
+import { useThemeStore } from '../../../store/themeStore';
 
 export default function Logout() {
+    const { isDark, } = useThemeStore();
     return (
         <>
             {/* Logout Button */}
             <div className="mt-auto pt-10 z-10 flex flex-col items-center gap-4">
-                <button className="cursor-pointer flex items-center gap-2 text-slate-500 hover:text-red-400 transition-all font-black uppercase tracking-[0.3em] text-[10px] py-3 px-10 border border-slate-800 rounded-full hover:border-red-400/50 bg-slate-900/80 backdrop-blur-md">
+                <button className={`
+                    cursor-pointer flex items-center gap-2 transition-all font-black uppercase 
+                    tracking-[0.3em] text-[10px] py-3 px-10 border rounded-full backdrop-blur-md
+                    ${isDark
+                        ? 'text-slate-500 hover:text-red-400 border-slate-800 bg-slate-900/80 hover:border-red-400/50 shadow-lg shadow-black/20'
+                        : 'text-slate-400 hover:text-red-500 border-slate-200 bg-white/80 hover:border-red-500/50 shadow-md shadow-slate-200'}
+                `}>
                     <HiOutlineLogout size={16} />
                     Selesaikan Shift
                 </button>
-                <p className="text-slate-700 text-[10px] font-bold uppercase">{name_apps} POS ENGINE V1.0.2</p>
+
+                <p className={`
+                    text-[10px] font-bold uppercase tracking-wider transition-colors
+                    ${isDark ? 'text-slate-700' : 'text-slate-400'}
+                `}>
+                    {name_apps} pos engine {version_apps}
+                </p>
             </div>
         </>
     );
