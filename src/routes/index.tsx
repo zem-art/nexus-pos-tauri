707eb50850a1d3856ccbda0035384d14703628c6
zem-art <amonsefra13@gmail.com>
2026-01-23 16:04:11 +0700

    create private routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import { PrivateRoute } from './PrivateRoute';
+import LoginPage from '../pages/auth/sign-in';
+import Dashboard from '../pages/dashboard';
+
+// import POSPage from '@/apps/pos/POSPage';
+
+export const AppRouter = () => (
+    <Routes>
+        {/* Rute Bebas Akses */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Rute Terproteksi (Harus Login) */}
+        <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/pos" element={<POSPage />} /> */}
+        </Route>
+    </Routes>
+);
