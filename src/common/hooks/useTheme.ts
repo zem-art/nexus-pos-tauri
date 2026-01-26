ce07cdb607e1870ab38b1aad3d61226dd49bac02
zem-art <amonsefra13@gmail.com>
2026-01-26 11:20:23 +0700

    remove coment in code

--- src/common/hooks/useTheme.ts
+++ src/common/hooks/useTheme.ts
@@ -1,21 +1,21 @@
-// // src/common/hooks/useTheme.tsx
-// import { useState, useEffect } from 'react';
+// src/common/hooks/useTheme.tsx
+import { useState, useEffect } from 'react';
 
-// export function useTheme() {
-//   const [isDark, setIsDark] = useState(() => {
-//     return localStorage.getItem('theme') === 'dark';
-//   });
+export function useTheme() {
+  const [isDark, setIsDark] = useState(() => {
+    return localStorage.getItem('theme') === 'dark';
+  });
 
-//   useEffect(() => {
-//     const root = window.document.documentElement;
-//     if (isDark) {
-//       root.classList.add('dark');
-//       localStorage.setItem('theme', 'dark');
-//     } else {
-//       root.classList.remove('dark');
-//       localStorage.setItem('theme', 'light');
-//     }
-//   }, [isDark]);
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (isDark) {
+      root.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      root.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDark]);
 
-//   return [isDark, setIsDark] as const;
-// }
+  return [isDark, setIsDark] as const;
+}
